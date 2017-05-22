21002269f48c0490cc88064724ca32a8fcc09261
Mikael Brevik <mikaelbre@gmail.com>
2017-05-22 19:00:34 +0200

    Removes uncommented code

--- token.ml
+++ token.ml
@@ -1,21 +1,9 @@
-
-(*type number =
-  |	Int of int
-  |	Big_int of Big_int.big_int
-  | Float of float
-
-  type number_type =
-  | BINARY of number
-  | OCTAL of number
-  | NORMAL of number*)
-
 type t =
     T_IDENTIFIER
   | T_LITERAL
   | T_OBJECT
   | T_ARRAY
   | T_NUMBER of string
-  (*| T_NUMBER_EXPONENT of string*)
   | T_STRING of string
   | T_BOOL of bool
   | T_COMMA
@@ -29,12 +17,6 @@ type t =
 
 let token_to_string = function
   | T_NUMBER n -> "T_NUMBER " ^ n
-  (*| T_NUMBER_EXPONENT n -> "T_NUMBER " ^ n*)
-  (*| T_NUMBER n -> let i = match n with
-      | BINARY o -> "T_NUMBER BINARY " ^ string_of_num o
-      | OCTAL o -> "T_NUMBER OCTAL " ^ string_of_num o
-      | NORMAL o -> "T_NUMBER NORMAL " ^ string_of_num o in
-    i*)
   | T_STRING x -> "T_STRING " ^ x
   | T_BOOL b -> "T_BOOL " ^ string_of_bool b
   | T_IDENTIFIER -> "T_IDENTIFIER"
